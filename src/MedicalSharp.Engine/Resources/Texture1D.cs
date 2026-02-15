e1e1ab581d1b790d86e230baad5cc6a0ff44a3fa
Lee <lishilei0523@163.com>
2026-02-15 18:19:37 +0800

    1D纹理；

---
+++ src/MedicalSharp.Engine/Resources/Texture1D.cs
@@ -0,0 +1,121 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using System;
+
+namespace MedicalSharp.Engine.Resources
+{
+    /// <summary>
+    /// 1D纹理
+    /// </summary>
+    public class Texture1D : Texture
+    {
+        #region # 字段及构造器
+
+        /// <summary>
+        /// 创建1D纹理构造器
+        /// </summary>
+        /// <param name="width">宽度</param>
+        /// <param name="pixelInternalFormat">像素内部格式</param>
+        public Texture1D(int width, PixelInternalFormat pixelInternalFormat = PixelInternalFormat.Rgba32f)
+            : base()
+        {
+            this.Width = width;
+
+            //绑定纹理、设置参数
+            GL.BindTexture(TextureTarget.Texture1D, base.Id);
+            GL.TexParameter(TextureTarget.Texture1D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.ClampToEdge);
+            GL.TexParameter(TextureTarget.Texture1D, TextureParameterName.TextureMinFilter, (int)TextureMagFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture1D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+
+            //上传纹理至显存
+            GL.TexImage1D(TextureTarget.Texture1D, 0, pixelInternalFormat, this.Width, 0, PixelFormat.Rgba, PixelType.Float, IntPtr.Zero);
+
+            //解绑
+            GL.BindTexture(TextureTarget.Texture1D, 0);
+        }
+
+        #endregion
+
+        #region # 属性
+
+        #region 宽度 —— int Width
+        /// <summary>
+        /// 宽度
+        /// </summary>
+        public int Width { get; private set; }
+        #endregion 
+
+        #endregion
+
+        #region # 方法
+
+        #region 更新纹理 —— void Update(IntPtr data)
+        /// <summary>
+        /// 更新纹理
+        /// </summary>
+        /// <param name="data">纹理数据</param>
+        public void Update(IntPtr data)
+        {
+            if (base.Id == 0 || data == IntPtr.Zero)
+            {
+                return;
+            }
+
+            GL.BindTexture(TextureTarget.Texture1D, base.Id);
+            GL.TexSubImage1D(TextureTarget.Texture1D, 0, 0, this.Width, PixelFormat.Rgba, PixelType.Float, data);
+            GL.BindTexture(TextureTarget.Texture1D, 0);
+        }
+        #endregion
+
+        #region 设置过滤器 —— abstract void SetFilter(TextureMinFilter minFilter...
+        /// <summary>
+        /// 设置过滤器
+        /// </summary>
+        /// <param name="minFilter">最小值过滤器</param>
+        /// <param name="magFilter">最大值过滤器</param>
+        public override void SetFilter(TextureMinFilter minFilter, TextureMagFilter magFilter)
+        {
+            GL.BindTexture(TextureTarget.Texture1D, base.Id);
+            GL.TexParameter(TextureTarget.Texture1D, TextureParameterName.TextureMinFilter, (int)minFilter);
+            GL.TexParameter(TextureTarget.Texture1D, TextureParameterName.TextureMagFilter, (int)magFilter);
+            GL.BindTexture(TextureTarget.Texture1D, 0);
+        }
+        #endregion
+
+        #region 设置包裹模式 —— abstract void SetWrapMode(TextureWrapMode wrapMode)
+        /// <summary>
+        /// 设置包裹模式
+        /// </summary>
+        /// <param name="wrapMode">包裹模式</param>
+        public override void SetWrapMode(TextureWrapMode wrapMode)
+        {
+            GL.BindTexture(TextureTarget.Texture1D, base.Id);
+            GL.TexParameter(TextureTarget.Texture1D, TextureParameterName.TextureWrapS, (int)wrapMode);
+            GL.BindTexture(TextureTarget.Texture1D, 0);
+        }
+        #endregion
+
+        #region 绑定纹理 —— abstract void Bind(int index)
+        /// <summary>
+        /// 绑定纹理
+        /// </summary>
+        /// <param name="index">纹理索引</param>
+        public override void Bind(int index)
+        {
+            GL.ActiveTexture(TextureUnit.Texture0 + index);
+            GL.BindTexture(TextureTarget.Texture1D, base.Id);
+        }
+        #endregion
+
+        #region 解绑纹理 —— abstract void Unbind()
+        /// <summary>
+        /// 解绑纹理
+        /// </summary>
+        public override void Unbind()
+        {
+            GL.BindTexture(TextureTarget.Texture1D, 0);
+        }
+        #endregion
+
+        #endregion
+    }
+}
