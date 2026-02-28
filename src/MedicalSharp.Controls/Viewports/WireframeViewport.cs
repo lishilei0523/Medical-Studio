3a60f935d2f1cf74a121d1a21bbe32ab85503e96
Lee <lishilei0523@163.com>
2026-02-28 23:58:33 +0800

    -

--- src/MedicalSharp.Controls/Viewports/WireframeViewport.cs
+++ src/MedicalSharp.Controls/Viewports/WireframeViewport.cs
@@ -1,5 +1,10 @@
 ﻿using Avalonia;
+using Avalonia.Collections;
+using Avalonia.Metadata;
 using MedicalSharp.Controls.Base;
+using MedicalSharp.Controls.Visuals;
+using MedicalSharp.Engine.Cameras;
+using MedicalSharp.Engine.Renderers;
 
 namespace MedicalSharp.Controls.Viewports
 {
@@ -8,7 +13,39 @@ namespace MedicalSharp.Controls.Viewports
     /// </summary>
     public class WireframeViewport : OpenTKViewport
     {
-        //TODO 实现
+
+        /// <summary>
+        /// 深度依赖属性
+        /// </summary>
+        public static readonly StyledProperty<OrbitCamera> CameraProperty;
+
+        /// <summary>
+        /// 静态构造器
+        /// </summary>
+        static WireframeViewport()
+        {
+            CameraProperty = AvaloniaProperty.Register<OpenTKViewport, OrbitCamera>(nameof(Camera));
+        }
+
+        /// <summary>
+        /// 线框渲染器
+        /// </summary>
+        private WireframeRenderer _renderer;
+
+        /// <summary>
+        /// 默认构造器
+        /// </summary>
+        public WireframeViewport()
+        {
+            this.Children = new AvaloniaList<Visual3D>();
+        }
+
+
+        /// <summary>
+        /// 子元素列表
+        /// </summary>
+        [Content]
+        public AvaloniaList<Visual3D> Children { get; }
 
         #region OpenTK渲染事件 —— override void OnOpenTKRender(PixelSize viewportSize)
         /// <summary>
