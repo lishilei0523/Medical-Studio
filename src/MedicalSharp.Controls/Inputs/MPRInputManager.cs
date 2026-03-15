f18ab15e7b96770e202cfba1ae4cabdaa7964973
Lee <lishilei0523@163.com>
2026-03-15 22:40:14 +0800

    MPR滚轮切换切片；

--- src/MedicalSharp.Controls/Inputs/MPRInputManager.cs
+++ src/MedicalSharp.Controls/Inputs/MPRInputManager.cs
@@ -1,4 +1,6 @@
-﻿using MedicalSharp.Engine.Cameras;
+﻿using MedicalSharp.Controls.Base;
+using MedicalSharp.Engine.Cameras;
+using System;
 
 namespace MedicalSharp.Controls.Inputs
 {
@@ -7,8 +9,11 @@ namespace MedicalSharp.Controls.Inputs
     /// </summary>
     public class MPRInputManager : InputManager
     {
-        //TODO 实现
+        #region # 字段及构造器
 
+        /// <summary>
+        /// MPR相机
+        /// </summary>
         private readonly MPRCamera _mprCamera;
 
         /// <summary>
@@ -18,5 +23,39 @@ namespace MedicalSharp.Controls.Inputs
         {
             this._mprCamera = mprCamera;
         }
+
+        #endregion
+
+        #region # 属性
+
+        #region 只读属性 - MPR相机 —— MPRCamera Camera
+        /// <summary>
+        /// 只读属性 - MPR相机
+        /// </summary>
+        public MPRCamera Camera
+        {
+            get => this._mprCamera;
+        }
+        #endregion
+
+        #endregion
+
+        #region # 方法
+
+        #region 鼠标滚轮事件 —— override void OnMouseWheel(OpenTKViewport viewport, double offsetX...
+        /// <summary>
+        /// 鼠标滚轮事件
+        /// </summary>
+        /// <param name="viewport">OpenTK视口</param>
+        /// <param name="offsetX">X轴偏移量</param>
+        /// <param name="offsetY">Y轴偏移量</param>
+        public override void OnMouseWheel(OpenTKViewport viewport, double offsetX, double offsetY)
+        {
+            this._mprCamera.SliceIndex += (int)Math.Ceiling(offsetY);
+            viewport.RequestNextFrameRendering();
+        }
+        #endregion 
+
+        #endregion
     }
 }
