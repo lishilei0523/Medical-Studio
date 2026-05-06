1ac843294111c2e68d441992e80d2917290520f2
Lee <lishilei0523@163.com>
2026-05-06 20:42:42 +0800

    添加Caliburn事件；

---
+++ src/MedicalSharp.Client/Events/MPRPlaneChangedEvent.cs
@@ -0,0 +1,25 @@
+﻿using MedicalSharp.Controls.Visuals;
+using SD.Infrastructure.Avalonia.Caliburn.Base;
+
+namespace MedicalSharp.Client.Events
+{
+    /// <summary>
+    /// MPR平面变化事件
+    /// </summary>
+    public class MPRPlaneChangedEvent : CaliburnEvent
+    {
+        /// <summary>
+        /// 十字线
+        /// </summary>
+        public CrosshairVisual3D Crosshair { get; set; }
+
+        /// <summary>
+        /// 是否由同步触发
+        /// </summary>
+        /// <remarks>
+        /// true：其他视图同步过来触发的，不应再往外广播
+        /// false：本视图用户操作触发的，应该通知其他视图
+        /// </remarks>
+        public bool IsSyncTriggered { get; set; }
+    }
+}
---
+++ src/MedicalSharp.Client/Events/ShapeTranslatingEvent.cs
@@ -0,0 +1,16 @@
+﻿using MedicalSharp.Controls.Visuals;
+using SD.Infrastructure.Avalonia.Caliburn.Base;
+
+namespace MedicalSharp.Client.Events
+{
+    /// <summary>
+    /// 形状平移中事件
+    /// </summary>
+    public class ShapeTranslatingEvent : CaliburnEvent
+    {
+        /// <summary>
+        /// 形状
+        /// </summary>
+        public ShapeVisual3D Shape { get; set; }
+    }
+}
