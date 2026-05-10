a4f471db0d70100def356f8eab15c1c1e1734755
Lee <lishilei0523@163.com>
2026-05-10 14:34:40 +0800

    添加MPR渲染模式枚举；

---
+++ src/MedicalSharp.Primitives/Enums/MPRRenderMode.cs
@@ -0,0 +1,18 @@
+﻿namespace MedicalSharp.Primitives.Enums
+{
+    /// <summary>
+    /// MPR渲染模式
+    /// </summary>
+    public enum MPRRenderMode : byte
+    {
+        /// <summary>
+        /// 灰度
+        /// </summary>
+        Gray = 0,
+
+        /// <summary>
+        /// 伪彩
+        /// </summary>
+        PseudoColor = 1
+    }
+}
